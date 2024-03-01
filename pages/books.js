f81c16f91890e48b63171a22a01bc06da2f5ce57
Jaiden Ratti <jaidenratti@gmail.com>
2024-03-01 02:46:35 -0500

    Update books.js

--- pages/books.js
+++ pages/books.js
@@ -415,7 +415,7 @@ export default function Books() {
         <h2 className={styles.title}>Bookshelf</h2>
         <p className={styles['book-desc']}>A reader lives a thousand lives</p>
 
-        <p className={styles.description}>Currently reading cognitive science, philosophy, epistemology, startups.
+        <p className={styles.description}>Currently reading philosophy, neuroscience, startups.
         Usually updating on Goodreads <Link href="https://www.goodreads.com/review/list/155376622?shelf=read"className={styles.customLink} target="_blank">
             @jaidenratti</Link> or Bookshelved <Link href="https://bookshelved.com/@jaiden"className={styles.customLink} target="_blank">@jaiden</Link>.</p>
 
